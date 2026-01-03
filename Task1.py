0839ae82a4fd801383c43ee0ba0fee8e0ca5e3d3
Shilisha1994 <shilishac@gmail.com>
2026-01-03 18:27:35 +0530

    Add files via upload
    Python assignment submission

---
+++ Task1.py
@@ -0,0 +1,14 @@
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# perform calculations
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+
+print("\nAddition:", addition)
+print("Subtraction:", subtraction)
+print("Multiplication:", multiplication)
+print("Division:", division)
+
